21faba13c3448a0b6bbd3c0524c5a0fbfc3ec7ae
Aaryan <sonofnaleen@gmail.com>
2024-03-14 15:59:20 +0545

    Header and Footer UI done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/company_logo.png";
 
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-white shadow dark:bg-gray-900 ">
+      <div className="w-1/2 mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Link
+            to="/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <img src={Logo} className="h-10" alt="Company Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Cinemate
+            </span>
+          </Link>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/aaryan-sharma09/"
+                className="hover:underline me-4 md:me-6"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://github.com/AaryanSharmaNeupane"
+                className="hover:underline"
+                target="_blank"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <span className="block mt-3 text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024
+          <Link to="/" className="hover:underline">
+            Cinemate™
+          </Link>
+          . All Rights Reserved.
+          <p>Rustu's Tech</p>
+        </span>
+      </div>
+    </footer>
+  );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,49 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/company_logo.png";
 
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="flex p-3 items-center justify-between">
+        <div className="flex items-center cursor-pointer">
+          <img className="w-12 " src={Logo} alt="Company logo" />
+          <h1 className="text-2xl ml-2 font-semibold">Cinemate</h1>
+        </div>
+        <nav className="w-1/3">
+          <ul className="flex justify-between">
+            <li>
+              <NavLink to="/" end>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="movies/popular">Popular</NavLink>
+            </li>
+            <li>
+              <NavLink to="movies/top">Top Rated</NavLink>
+            </li>
+            <li>
+              <NavLink to="movies/upcoming">Upcoming</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex ">
+          <button className="border px-4 rounded-lg mr-4 hover:border-slate-500">
+            <i class="bi bi-moon-fill"></i>
+          </button>
+          <div className="border p-2 rounded-lg hover:border-slate-500 focus-within:border-slate-500 ">
+            <i class="bi bi-search "></i>
+            <input
+              className=" outline-none ml-2"
+              placeholder=" Search..."
+              autoComplete="off"
+              name="search"
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
