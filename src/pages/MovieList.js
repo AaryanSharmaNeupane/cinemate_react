27ba25909edb2d3a8ba11f605b9d9159e1a00616
Aaryan <sonofnaleen@gmail.com>
2024-03-15 13:43:15 +0545

    Data fetched from API and navbar adjusted accordingly

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Card } from "../components/Card";
+import { useFetch } from "../hooks/useFetch";
 
-export const MovieList = () => {
+export const MovieList = (apiPath) => {
+  const { data: movies } = useFetch(apiPath);
+
   return (
     <main>
       <section className="max-w-7xl m-auto py-7">
         <div className="flex justify-start flex-wrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
